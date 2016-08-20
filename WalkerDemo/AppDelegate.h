75d355665499a85cf1120d7b492777b087a6a126
guang <331689079@qq.com>
2016-08-20 10:32:46 +0800

    Initial Commit

---
+++ WalkerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WalkerDemo
+//
+//  Created by 光 on 16/8/20.
+//  Copyright © 2016年 光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WalkerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WalkerDemo
+//
+//  Created by 光 on 16/8/20.
+//  Copyright © 2016年 光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
